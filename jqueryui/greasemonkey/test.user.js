6dad59554712731ec36921e84b286bc8789d60d1
greentea039 <lemontea053-eh@yahoo.co.jp>
2012-12-24 14:43:15 +0900

    Fix bugs

--- jqueryui/greasemonkey/test.user.js
+++ jqueryui/greasemonkey/test.user.js
@@ -3,25 +3,25 @@
 // @namespace       test
 // @version         1.0
 // @include         http://*
+// @include         https://*
 // @delay 20
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require         https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js
-// @resource        jQueryUICSS                                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/jquery-ui.css
-// @resource        ui-bg_diagonals-thick_18_b81900_40x40.png   https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_diagonals-thick_18_b81900_40x40.png
-// @resource        ui-bg_diagonals-thick_20_666666_40x40.png   https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_diagonals-thick_20_666666_40x40.png
-// @resource        ui-bg_flat_10_000000_40x100.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_flat_10_000000_40x100.png
-// @resource        ui-bg_glass_100_f6f6f6_1x400.png            https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_glass_100_f6f6f6_1x400.png
-// @resource        ui-bg_glass_100_fdf5ce_1x400.png            https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_glass_100_fdf5ce_1x400.png
-// @resource        ui-bg_glass_65_ffffff_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_glass_65_ffffff_1x400.png
-// @resource        ui-bg_gloss-wave_35_f6a828_500x100.png      https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_gloss-wave_35_f6a828_500x100.png
-// @resource        ui-bg_highlight-soft_100_eeeeee_1x100.png   https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_highlight-soft_100_eeeeee_1x100.png
-// @resource        ui-bg_highlight-soft_75_ffe45c_1x100.png    https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-bg_highlight-soft_75_ffe45c_1x100.png
-// @resource        ui-icons_222222_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-icons_222222_256x240.png
-// @resource        ui-icons_228ef1_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-icons_228ef1_256x240.png
-// @resource        ui-icons_ef8c08_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-icons_ef8c08_256x240.png
-// @resource        ui-icons_ffd27a_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-icons_ffd27a_256x240.png
-// @resource        ui-icons_ffffff_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/ui-lightness/images/ui-icons_ffffff_256x240.png
-// @resource        htmlFlagment     https://raw.github.com/greentea039/test/master/jqueryui/greasemonkey/resources/fragment.html
+// @resource        jQueryUICSS                                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/jquery-ui.css
+// @resource        ui-bg_flat_0_aaaaaa_40x100.png              https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_flat_0_aaaaaa_40x100.png
+// @resource        ui-bg_flat_75_ffffff_40x100.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_flat_75_ffffff_40x100.png
+// @resource        ui-bg_glass_55_fbf9ee_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_55_fbf9ee_1x400.png
+// @resource        ui-bg_glass_65_ffffff_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_65_ffffff_1x400.png
+// @resource        ui-bg_glass_75_dadada_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_75_dadada_1x400.png
+// @resource        ui-bg_glass_75_e6e6e6_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_75_e6e6e6_1x400.png
+// @resource        ui-bg_glass_95_fef1ec_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_95_fef1ec_1x400.png
+// @resource        ui-bg_highlight-soft_75_cccccc_1x100.png    https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_highlight-soft_75_cccccc_1x100.png
+// @resource        ui-icons_222222_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_222222_256x240.png
+// @resource        ui-icons_2e83ff_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_2e83ff_256x240.png
+// @resource        ui-icons_454545_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_454545_256x240.png
+// @resource        ui-icons_888888_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_888888_256x240.png
+// @resource        ui-icons_cd0a0a_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_cd0a0a_256x240.png
+// @resource        htmlFragment     https://raw.github.com/greentea039/test/master/jqueryui/greasemonkey/resources/fragment.html
 // @run-at          document-end
 // ==/UserScript==
 					
@@ -29,7 +29,7 @@ function appendButton() {
 	var div = document.createElement("div");
 	div.setAttribute("id", "extensionButton");
 	div.setAttribute("class", "ui-state-default ui-corner-all");
-	div.style.cssText = "position:absolute; top: 20px; left: 20px; z-index: 9999; cursor: pointer;";
+	div.style.cssText = "position:absolute; top: 20px; left: 20px; z-index: 999; cursor: pointer;";
 	div.innerHTML = '<span style="margin: 3px" class="ui-icon ui-icon-wrench" />';
 	document.body.appendChild(div);
 	div.addEventListener("click", openDialog);
@@ -37,44 +37,43 @@ function appendButton() {
 
 function openDialog(event) {
 	var div = document.createElement("div");
+	document.body.appendChild(div);
+	div.setAttribute("id", "dialog-modal");
+	div.setAttribute("title", "Basic modal dialog");
 	div.innerHTML = GM_getResourceText("htmlFragment");
 	$(div).dialog({
-		autoOpen: false,
-		closeOnEscape: true,
-		draggable: true,
-		resizable: true,
-		height: 400,
-		width: 400,
 		modal: true,
-		position: ["center", "center"],
-		title: "test"
 	});
 	$(div).dialog("open");
 }
 
+function escapeRegex(value) {
+	return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
+}
+
 function addUIStyle() {
 	var resourceNames = [
-		"ui-bg_diagonals-thick_18_b81900_40x40.png",
-		"ui-bg_diagonals-thick_20_666666_40x40.png",
-		"ui-bg_flat_10_000000_40x100.png",
-		"ui-bg_glass_100_f6f6f6_1x400.png",
-		"ui-bg_glass_100_fdf5ce_1x400.png",
+		"ui-bg_flat_0_aaaaaa_40x100.png",
+		"ui-bg_flat_75_ffffff_40x100.png",
+		"ui-bg_glass_55_fbf9ee_1x400.png",
 		"ui-bg_glass_65_ffffff_1x400.png",
-		"ui-bg_gloss-wave_35_f6a828_500x100.png",
-		"ui-bg_highlight-soft_100_eeeeee_1x100.png",
-		"ui-bg_highlight-soft_75_ffe45c_1x100.png",
+		"ui-bg_glass_75_dadada_1x400.png",
+		"ui-bg_glass_75_e6e6e6_1x400.png",
+		"ui-bg_glass_95_fef1ec_1x400.png",
+		"ui-bg_highlight-soft_75_cccccc_1x100.png",
 		"ui-icons_222222_256x240.png",
-		"ui-icons_228ef1_256x240.png",
-		"ui-icons_ef8c08_256x240.png",
-		"ui-icons_ffd27a_256x240.png",
-		"ui-icons_ffffff_256x240.png",
+		"ui-icons_2e83ff_256x240.png",
+		"ui-icons_454545_256x240.png",
+		"ui-icons_888888_256x240.png",
+		"ui-icons_cd0a0a_256x240.png",
 	];
 	var css = GM_getResourceText("jQueryUICSS")
 	for (var i = 0; i < resourceNames.length; i++) {
 		var resourceName = resourceNames[i];
 		var resourceURL = GM_getResourceURL(resourceName);
-		console.debug(resourceURL);
-		css = css.replace("images/" + resourceName, resourceURL);
+		console.debug(resourceName + ": " + resourceURL);
+		var regex = new RegExp(escapeRegex("images/" + resourceName), "g");
+		css = css.replace(regex, resourceURL);
 	}
 	GM_addStyle(css);
 }
