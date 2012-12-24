44f50374db17cdff378abb0d0571d44ff4d36779
greentea039 <lemontea053-eh@yahoo.co.jp>
2012-12-25 00:48:10 +0900

    Fix to be available as Chrome extension

--- jqueryui/greasemonkey/test.user.js
+++ jqueryui/greasemonkey/test.user.js
@@ -2,12 +2,12 @@
 // @name            test
 // @namespace       test
 // @version         1.0
-// @include         http://*
-// @include         https://*
+// @include         http://*/*
+// @include         https://*/*
 // @delay 20
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require         https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js
-// @resource        jQueryUICSS                                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/jquery-ui.css
+// @resource        jquery-ui.css                               https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/jquery-ui.css
 // @resource        ui-bg_flat_0_aaaaaa_40x100.png              https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_flat_0_aaaaaa_40x100.png
 // @resource        ui-bg_flat_75_ffffff_40x100.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_flat_75_ffffff_40x100.png
 // @resource        ui-bg_glass_55_fbf9ee_1x400.png             https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-bg_glass_55_fbf9ee_1x400.png
@@ -21,16 +21,84 @@
 // @resource        ui-icons_454545_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_454545_256x240.png
 // @resource        ui-icons_888888_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_888888_256x240.png
 // @resource        ui-icons_cd0a0a_256x240.png                 https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/images/ui-icons_cd0a0a_256x240.png
-// @resource        htmlFragment     https://raw.github.com/greentea039/test/master/jqueryui/greasemonkey/resources/fragment.html
+// @resource        fragment.html                               https://raw.github.com/greentea039/test/master/jqueryui/greasemonkey/resources/fragment.html
 // @run-at          document-end
 // ==/UserScript==
-					
+
+var util = {
+	isChrome: navigator.userAgent.indexOf("Chrome") >= 0,
+
+	escapeRegex: function (value) {
+		return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
+	},
+};
+
+var browserExtension = {
+	ImageResourceInfo: function (originalPath, name, resourcePath) {
+		this.originalPath = originalPath;
+		this.name = name;
+		this.resourcePath = resourcePath;
+	},
+	getResourceURL: function (resourcePath, resourceName) {
+		var resourceURL;
+		if (util.isChrome) {
+			resourceURL = chrome.extension.getURL(resourcePath + resourceName);
+		} else {
+			resourceURL = GM_getResourceURL(resourceName);
+		}
+		return resourceURL;
+	},
+	requestResourceText: function (resoucePath, resourceName, callback) {
+		if (util.isChrome) {
+			var request = new XMLHttpRequest();
+			var resourceURL = browserExtension.getResourceURL(resoucePath, resourceName);
+			request.open("GET", resourceURL);
+			request.onreadystatechange = function (event) {
+				if (request.readyState === 4) {
+					if (request.status === 200) {
+						if (callback instanceof Function) {
+							callback(request.responseText);
+						}
+					} else {
+						console.log("Error loading page, status = " + request.status + ", responseText = [" + request.responseText + "]");
+					}
+				}
+			};
+			request.send(null);
+		} else {
+			if (callback instanceof Function) {
+				callback(GM_getResourceText(resourceName));
+			}
+		}
+	},
+	addStyleFromResource: function (styleResourcePath, styleResouceName, imageResouceInfoArray) {
+		browserExtension.requestResourceText(styleResourcePath, styleResouceName, function (styleText) {
+			// replace image URLs
+			for (var i = 0; i < imageResouceInfoArray.length; i++) {
+				var imageResourceName = imageResouceInfoArray[i].name;
+				var imageOriginalPath = imageResouceInfoArray[i].originalPath;
+				var imageResourcePath = imageResouceInfoArray[i].resourcePath;
+				var imageResourceURL = browserExtension.getResourceURL(imageResourcePath, imageResourceName);
+				var regex = new RegExp(util.escapeRegex(imageOriginalPath + imageResourceName), "g");
+				styleText = styleText.replace(regex, imageResourceURL);
+			}
+			// add style
+			if (util.isChrome) {
+				var styleElement = document.createElement("style");
+				styleElement.textContent = styleText;
+				document.documentElement.appendChild(styleElement);
+			} else {
+				GM_addStyle(styleText);
+			}
+		});
+	},
+}
+
 function appendButton() {
 	var div = document.createElement("div");
 	div.setAttribute("id", "extensionButton");
-	div.setAttribute("class", "ui-state-default ui-corner-all");
-	div.style.cssText = "position:absolute; top: 20px; left: 20px; z-index: 999; cursor: pointer;";
-	div.innerHTML = '<span style="margin: 3px" class="ui-icon ui-icon-wrench" />';
+	div.setAttribute("class", "ui-state-default ui-corner-all ui-icon ui-icon-wrench");
+	div.style.cssText = "position: absolute; top: 20px; left: 20px; z-index: 999; cursor: pointer;";
 	document.body.appendChild(div);
 	div.addEventListener("click", openDialog);
 }
@@ -40,42 +108,34 @@ function openDialog(event) {
 	document.body.appendChild(div);
 	div.setAttribute("id", "dialog-modal");
 	div.setAttribute("title", "Basic modal dialog");
-	div.innerHTML = GM_getResourceText("htmlFragment");
 	$(div).dialog({
 		modal: true,
 	});
 	$(div).dialog("open");
-}
 
-function escapeRegex(value) {
-	return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
+	browserExtension.requestResourceText("resources/", "fragment.html", function (resourceText) {
+		div.innerHTML = resourceText;
+	});
 }
 
 function addUIStyle() {
-	var resourceNames = [
-		"ui-bg_flat_0_aaaaaa_40x100.png",
-		"ui-bg_flat_75_ffffff_40x100.png",
-		"ui-bg_glass_55_fbf9ee_1x400.png",
-		"ui-bg_glass_65_ffffff_1x400.png",
-		"ui-bg_glass_75_dadada_1x400.png",
-		"ui-bg_glass_75_e6e6e6_1x400.png",
-		"ui-bg_glass_95_fef1ec_1x400.png",
-		"ui-bg_highlight-soft_75_cccccc_1x100.png",
-		"ui-icons_222222_256x240.png",
-		"ui-icons_2e83ff_256x240.png",
-		"ui-icons_454545_256x240.png",
-		"ui-icons_888888_256x240.png",
-		"ui-icons_cd0a0a_256x240.png",
+	var Obj = browserExtension.ImageResourceInfo;
+	var imageResouceInfoArray = [
+		new Obj("images/", "ui-bg_flat_0_aaaaaa_40x100.png", "images/"),
+		new Obj("images/", "ui-bg_flat_75_ffffff_40x100.png", "images/"),
+		new Obj("images/", "ui-bg_glass_55_fbf9ee_1x400.png", "images/"),
+		new Obj("images/", "ui-bg_glass_65_ffffff_1x400.png", "images/"),
+		new Obj("images/", "ui-bg_glass_75_dadada_1x400.png", "images/"),
+		new Obj("images/", "ui-bg_glass_75_e6e6e6_1x400.png", "images/"),
+		new Obj("images/", "ui-bg_glass_95_fef1ec_1x400.png", "images/"),
+		new Obj("images/", "ui-bg_highlight-soft_75_cccccc_1x100.png", "images/"),
+		new Obj("images/", "ui-icons_222222_256x240.png", "images/"),
+		new Obj("images/", "ui-icons_2e83ff_256x240.png", "images/"),
+		new Obj("images/", "ui-icons_454545_256x240.png", "images/"),
+		new Obj("images/", "ui-icons_888888_256x240.png", "images/"),
+		new Obj("images/", "ui-icons_cd0a0a_256x240.png", "images/"),
 	];
-	var css = GM_getResourceText("jQueryUICSS")
-	for (var i = 0; i < resourceNames.length; i++) {
-		var resourceName = resourceNames[i];
-		var resourceURL = GM_getResourceURL(resourceName);
-		console.debug(resourceName + ": " + resourceURL);
-		var regex = new RegExp(escapeRegex("images/" + resourceName), "g");
-		css = css.replace(regex, resourceURL);
-	}
-	GM_addStyle(css);
+	browserExtension.addStyleFromResource("resources/", "jquery-ui.css", imageResouceInfoArray);
 }
 
 addUIStyle();
